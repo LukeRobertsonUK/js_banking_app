06ca603104bba6a41380f6b68922e3ed484834a4
Luke Robertson <luke.robertson@mac.com>
2013-07-25 22:54:25 +0100

    adds overdraft protection and coloured text on zero

--- js/main.js
+++ js/main.js
@@ -4,55 +4,74 @@ $(function() {
   var balanceOne = 0;
   var balanceTwo = 0;
 
-  $('#deposit_one').on('click', function(e){
-    depositMoney('one', $('#amount_one').val());
-    $("#placeholder_one").remove();
-    $("#balance_one").append("<p id='placeholder_one'> $" + balanceOne + "</p>");
-  });
-
-  $('#deposit_two').on('click', function(e){
-    depositMoney('two', $('#amount_two').val());
-    $("#placeholder_two").remove();
-    $("#balance_two").append("<p id='placeholder_two'> $" + balanceTwo + "</p>");
-  });
-
   function depositMoney(account, amount){
     switch(account) {
       case 'one':
-        balanceOne += parseInt(amount);
+        balanceOne += amount;
+        $("#placeholder_one").remove();
+        $("#balance_one").append("<h2 id='placeholder_one'> $" + balanceOne + "</h2>");
       break;
       case 'two':
-        balanceTwo += parseInt(amount);
+        balanceTwo += amount;
+        $("#placeholder_two").remove();
+        $("#balance_two").append("<h2 id='placeholder_two'> $" + balanceTwo + "</h2>");
       break;
     };
   };
-
-
- $('#withdraw_one').on('click', function(e){
-    withdrawMoney('one', $('#amount_one').val());
-    $("#placeholder_one").remove();
-    $("#balance_one").append("<p id='placeholder_one'> $" + balanceOne + "</p>");
-  });
-
-  $('#withdraw_two').on('click', function(e){
-    withdrawMoney('two', $('#amount_two').val())
-    $("#placeholder_two").remove();
-    $("#balance_two").append("<p id='placeholder_two'> $" + balanceTwo + "</p>");
-  });
 
   function withdrawMoney(account, amount){
     switch(account) {
       case 'one':
-        if(parseInt(amount) <= balanceOne){
-          balanceOne -= parseInt(amount);
+        balanceOne -= amount;
+        $("#placeholder_one").remove();
+        $("#balance_one").append("<h2 id='placeholder_one'> $" + balanceOne + "</h2>");
+        if(balanceOne == 0){
+          $("#placeholder_one").addClass("zero");
         };
       break;
       case 'two':
-        if(parseInt(amount) <= balanceTwo){
-          balanceTwo -= parseInt(amount);
+        balanceTwo -= amount;
+        $("#placeholder_two").remove();
+        $("#balance_two").append("<h2 id='placeholder_two'> $" + balanceTwo + "</h2>");
+         if(balanceTwo == 0){
+          $("#placeholder_two").addClass("zero");
         };
       break;
     };
   };
+
+ $('#deposit_one').on('click', function(e){
+    depositMoney('one', parseInt($('#amount_one').val()));
+  });
+
+  $('#deposit_two').on('click', function(e){
+    depositMoney('two', parseInt($('#amount_two').val()));
+  });
+
+ $('#withdraw_one').on('click', function(e){
+    var amount = $('#amount_one').val()
+    if(amount > (balanceOne+balanceTwo)){
+    } else {
+      if(amount > balanceOne){
+        withdrawMoney('two', (amount - balanceOne));
+        withdrawMoney('one', balanceOne);
+      } else {
+        withdrawMoney('one', amount);
+      };
+    };
+  });
+
+ $('#withdraw_two').on('click', function(e){
+    var amount = $('#amount_two').val()
+    if(amount > (balanceOne+balanceTwo)){
+    } else {
+      if(amount > balanceTwo){
+        withdrawMoney('one', (amount - balanceTwo));
+        withdrawMoney('two', balanceTwo);
+      } else {
+        withdrawMoney('two', amount);
+      };
+    };
+  });
 
 });
